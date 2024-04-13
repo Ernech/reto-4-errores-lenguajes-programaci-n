58b9f05df08ebd9738013f62b9666d856cf7b64a
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-04-13 19:28:41 -0400

    Drawing paddle

--- javascript/breackout-game/breackout-script.js
+++ javascript/breackout-game/breackout-script.js
@@ -5,6 +5,30 @@ let y = canvas.height - 30;
 let dx = 2
 let dy = -2;
 let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+const keyDownHandler = (e) => {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    } else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+const keyUpHandler = (e) => {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    } else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 const drawBall = () => {
     ctx.beginPath();
@@ -13,20 +37,42 @@ const drawBall = () => {
     ctx.fill();
     ctx.closePath();
 }
+const drawPaddle = () => {
+
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+}
 
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    if(x+dx<ballRadius || x+dx> canvas.width-ballRadius){
-        dx=-dx;
+    drawPaddle();
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+        dx = -dx;
     }
-    if(y+dy<ballRadius || y+dy>canvas.height-ballRadius){
-        dy=-dy;
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+        dy = -dy;
     }
-   
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+
     x += dx;
     y += dy;
+
 }
+
+
+
+
 
 setInterval(draw, 10);
 
