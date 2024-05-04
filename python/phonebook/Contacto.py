fca901dbb080d88fabebc2bcbf9f9e2846fad4b7
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-05-04 19:44:13 -0400

    Fin phonebook

--- python/phonebook/Contacto.py
+++ python/phonebook/Contacto.py
@@ -1,25 +1,48 @@
 class Contacto:
-    
-    contactos=[]
-    
-    def __init__(self, nombre,numero):
+    contactos = []
+
+    def __init__(self, nombre, numero):
         self.nombre = nombre
         self.numero = numero
-    
-    def agregar_contacto(self,contacto):
-        self.contactos.append(contacto)
-        print(f"El contacto {contacto.nombre} fue agregado con éxito")
-    
-    def buscar_contacto(self,nombre):
-        encontrado=False
-        for contacto in self.contactos:
+
+    def agregar_contacto(self):
+        self.contactos.append(self)
+        print(f"El contacto {self.nombre} fue agregado con éxito")
+
+    @classmethod
+    def buscar_contacto(cls, nombre):
+        encontrado = False
+        for contacto in cls.contactos:
             if contacto.nombre == nombre:
                 print(f"Nombre: {contacto.nombre}, Telefono: {contacto.numero}")
                 encontrado = True
                 break
-        if encontrado:
-            pass
-        else:
+        if not encontrado:
             print(f"Contacto no encontrado")
 
+    @classmethod
+    def eliminar_contacto(cls, nombre):
+        for i, contacto in enumerate(cls.contactos):
+            if contacto.nombre == nombre:
+                del cls.contactos[i]
+                print(f"El contacto {nombre} fue eliminado con éxito")
+                return
+        print(f"Contacto {nombre} no encontrado")
+
+    @classmethod
+    def modificar_contacto(cls, nombre, nuevo_nombre, nuevo_numero):
+        for contacto in cls.contactos:
+            if contacto.nombre == nombre:
+                contacto.nombre = nuevo_nombre
+                contacto.numero = nuevo_numero
+                print(f"El contacto {nombre} fue modificado con éxito")
+                return
+        print(f"Contacto {nombre} no encontrado")
     
+    @classmethod
+    def ver_contactos(cls):
+        for contacto in cls.contactos:
+            print("=====================================")
+            print(f"Nombre {contacto.nombre}")
+            print(f"Numero {contacto.numero}")
+            
--- python/phonebook/main.py
+++ python/phonebook/main.py
@@ -1,7 +1,45 @@
+from Contacto import Contacto
+
 def menu():
-    print("Agenta telefónica")
+    print("Agenda telefónica")
     print("1. Agregar Contacto")
     print("2. Buscar Contacto")
     print("3. Modificar Contacto")
     print("4. Eliminar Contacto")
-    print("5. Salir")
+    print("5. Ver contactos")
+    print("6. Salir")
+
+fin_programa = False
+
+while not fin_programa:
+    menu()
+    try:
+        accion = int(input("Seleccione una opción: "))
+        match accion:
+            case 1:
+                nombre = input("Nombre del contacto: ")
+                numero = input("Número del contacto: ")
+                contacto = Contacto(nombre, numero)
+                contacto.agregar_contacto()
+            case 2:
+                nombre = input("Ingrese el nombre del contacto: ")
+                Contacto.buscar_contacto(nombre)
+            case 3:
+                nombre = input("Ingrese el nombre del contacto: ")
+                nombre_nuevo = input("Ingrese el nuevo nombre del contacto: ")
+                nuevo_numero = input("Ingrese el nuevo número del contacto: ")
+                Contacto.modificar_contacto(nombre, nombre_nuevo, nuevo_numero)
+            case 4: 
+                nombre = input("Ingrese el nombre del contacto: ")
+                Contacto.eliminar_contacto(nombre)
+            case 5:
+                print("Contactos:")
+                Contacto.ver_contactos()
+            case 6:
+                fin_programa = True
+                print("Saliendo del programa...")
+                break
+            case _:
+                print("Ingrese una instrucción válida")
+    except ValueError:
+        print("Ingrese un número válido para la opción.")
