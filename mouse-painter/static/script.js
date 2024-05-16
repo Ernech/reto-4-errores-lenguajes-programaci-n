b228c6539bd6ab7ff8261fe00e9d7cae400dfd1c
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-05-15 21:57:41 -0400

    Tools script

--- mouse-painter/static/script.js
+++ mouse-painter/static/script.js
@@ -41,3 +41,173 @@ const pencil = ()=>{
         canvasData.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
     }
 }
+function line(){
+           
+    canvas.onmousedown = function (e){
+        img = ctx.getImageData(0, 0, width, height);
+        prevX = e.clientX - canvas.offsetLeft;
+        prevY = e.clientY - canvas.offsetTop;
+        hold = true;
+    };
+            
+    canvas.onmousemove = function linemove(e){
+        if (hold){
+            ctx.putImageData(img, 0, 0);
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(curX, curY);
+            ctx.stroke();
+            canvasData.line.push({ "starx": prevX, "starty": prevY, "endx": curX, "endY": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+            ctx.closePath();
+        }
+    };
+            
+    canvas.onmouseup = function (e){
+         hold = false;
+    };
+            
+    canvas.onmouseout = function (e){
+         hold = false;
+    };
+}
+        
+        
+function rectangle(){
+            
+    canvas.onmousedown = function (e){
+        img = ctx.getImageData(0, 0, width, height);
+        prevX = e.clientX - canvas.offsetLeft;
+        prevY = e.clientY - canvas.offsetTop;
+        hold = true;
+    };
+            
+    canvas.onmousemove = function (e){
+        if (hold){
+            ctx.putImageData(img, 0, 0);
+            curX = e.clientX - canvas.offsetLeft - prevX;
+            curY = e.clientY - canvas.offsetTop - prevY;
+            ctx.strokeRect(prevX, prevY, curX, curY);
+            if (fill_value){
+                ctx.fillRect(prevX, prevY, curX, curY);
+            }
+            canvasData.rectangle.push({ "starx": prevX, "stary": prevY, "width": curX, "height": curY, "thick": ctx.lineWidth, "stroke": stroke_value, "stroke_color": ctx.strokeStyle, "fill": fill_value, "fill_color": ctx.fillStyle });
+            
+        }
+    };
+            
+    canvas.onmouseup = function(e){
+        hold = false;
+    };
+            
+    canvas.onmouseout = function(e){
+        hold = false;
+    };
+}
+        
+        
+function circle(){
+            
+    canvas.onmousedown = function (e){
+        img = ctx.getImageData(0, 0, width, height);
+        prevX = e.clientX - canvas.offsetLeft;
+        prevY = e.clientY - canvas.offsetTop;
+        hold = true;
+    };
+            
+    canvas.onmousemove = function (e){
+        if (hold){
+            ctx.putImageData(img, 0, 0);
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            ctx.beginPath();
+            ctx.arc(Math.abs(curX + prevX)/2, Math.abs(curY + prevY)/2, Math.sqrt(Math.pow(curX - prevX, 2) + Math.pow(curY - prevY, 2))/2, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.stroke();
+            if (fill_value){
+               ctx.fill();
+            }
+            canvasData.circle.push({ "starx": prevX, "stary": prevY, "radius": curX - prevX, "thick": ctx.lineWidth, "stroke": stroke_value, "stroke_color": ctx.strokeStyle, "fill": fill_value, "fill_color": ctx.fillStyle });
+        }
+    };
+            
+    canvas.onmouseup = function (e){
+        hold = false;
+    };
+            
+    canvas.onmouseout = function (e){
+        hold = false;
+    };
+}
+        
+        
+function eraser(){
+    
+    canvas.onmousedown = function(e){
+        curX = e.clientX - canvas.offsetLeft;
+        curY = e.clientY - canvas.offsetTop;
+        hold = true;
+            
+        prevX = curX;
+        prevY = curY;
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+    };
+        
+    canvas.onmousemove = function(e){
+        if(hold){
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    };
+        
+    canvas.onmouseup = function(e){
+        hold = false;
+    };
+        
+    canvas.onmouseout = function(e){
+        hold = false;
+    };
+        
+    function draw(){
+        ctx.lineTo(curX, curY);
+        ctx.strokeStyle = "#ffffff";
+        ctx.stroke();
+        canvasData.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+    }    
+}  
+
+function color(colorValue){
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+}    
+        
+function add_pixel(){
+    ctx.lineWidth += 1;
+}
+        
+function reduce_pixel(){
+    if (ctx.lineWidth == 1){
+        ctx.lineWidth = 1;
+    }
+    else{
+        ctx.lineWidth -= 1;
+    }
+}
+        
+function fill(){
+    fillValue = true;
+    strokeValue = false;
+}
+        
+function outline(){
+    fillValue = false;
+    strokeValue = true;
+}
+               
+function reset(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasData = { "pencil": [], "line": [], "rectangle": [], "circle": [], "eraser": [] }
+}
