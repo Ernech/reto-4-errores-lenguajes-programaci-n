916ccb8bc25f340b809368638c141a94b8a8206a
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-05-23 22:58:49 -0400

    Comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+#SET YOUR PROJECT PATH IN THE 'template_folder' AND THE 'static_folder' PARAMS
 app = Flask(__name__, template_folder='D:/DigitalNAO/reto-4/mouse-painter/templates',static_folder='D:/DigitalNAO/reto-4/mouse-painter/static', static_url_path='/static')
 
 @app.route("/")
